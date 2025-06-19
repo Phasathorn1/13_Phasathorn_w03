9eac5038e93545e822aa539a65cd6e3fa161ca0f
Phasathorn Mooljoy <pasatornta@hotmail.co.th>
2025-06-19 01:45:35 -0700

    add w06-07

---
+++ w06.py
@@ -0,0 +1,10 @@
+# list
+numbers = []
+
+colors = ["red","green","blue","Yellow"]
+numbers = [11,10,99,45,33,11]
+
+numbers_1_20 = list(range(1,20))
+numbers_1_100 = list(range(0,101,10))
+
+print(numbers_1_100)
---
+++ w07.py
@@ -0,0 +1,7 @@
+colors = ["red","green","blue","Yellow"]
+
+# print(f"{colors[0]} {colors[1]} {colors[2]} {colors[3]}")
+
+# print(f"{colors[0:4]}")
+
+print(f"{colors[-3:]}")
